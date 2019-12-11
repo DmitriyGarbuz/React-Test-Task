0771fcaa8062c93353c915fcd6d21ecc7a1836cd
DmitriyGarbuz <51447305+DmitriyGarbuz@users.noreply.github.com>
2019-12-11 09:38:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import img from './img/photo.jpg'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './home/Home';
+import Users from './users/Users';
+import OneUser from './user/OneUser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = (props) => {
+    return (
+     <Router>
+         <Route exact path = '/' component = {Home} />
+         <Route path = '/user' component = {Users} />
+         <Route path = '/user/:id' component = {OneUser} />
+     </Router>
+    )};
 
 export default App;
